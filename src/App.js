ee65e84992592aa69687625b7f46dc915d53a47b
rosso101 <victorprivv101@gmail.com>
2023-11-12 17:49:27 +0300

    added routes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ListItems from './ListItems';
 import Contacts from './components/Contacts';
-import './App.css'
+import './App.css';
 import Navbar from './components/Navbar';
 import HomePage from './components/HomePage';
 import HouseListingForm from './components/HouseListing';
 import About from './components/About';
-import Picture from './Picture.png'
+import Picture from './Picture.png';
+
 function App() {
   return (
-    <div className="App">
-      {/* Other components or content can be added here */}
-      <Navbar/>
-      <HomePage/>
-      <img src={Picture} alt="My Picture" />
-      <ListItems />
-      <About/>
-      <HouseListingForm/>
-      <Contacts/>
-
-
-
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/listings" element={<ListItems />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sell" element={<HouseListingForm />} />
+          <Route path="/contacts" element={<Contacts />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,17 @@
+// Navbar.js
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
     <nav className="naBar">
-         <nav className="naBar">
-        <ul className="HomeTopBar">
-          <button id="TheHomeBar">HOME</button>
-          <button id="HomeLocations">LOCATIONS</button>
-          <button id="about">ABOUT</button>
-          <button id="contacts">CONTACTS</button>
-          <button id="sell">SELL</button>
-        </ul>
-    </nav>
+      <ul className="HomeTopBar">
+        <li><button><Link to="/">HOME</Link></button></li>
+        <li><button><Link to="/listings">LISTINGS</Link></button></li>
+        <li><button><Link to="/about">ABOUT</Link></button></li>
+        <li><button><Link to="/contacts">CONTACTS</Link></button></li>
+        <li><button><Link to="/sell">SELL</Link></button></li>
+      </ul>
     </nav>
   );
 }
